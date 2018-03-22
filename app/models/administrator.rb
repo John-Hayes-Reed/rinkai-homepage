92589c1b8e4f758d588c12064628e5bfed0c24bc
John <john.hayes.reed@gmail.com>
2018-03-22 09:31:06 +0000

    fixed rubocop offences

--- app/models/administrator.rb
+++ app/models/administrator.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
+# class to model an administrative user of the application.
 class Administrator < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable

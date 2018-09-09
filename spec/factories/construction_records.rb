a8fcbe36c7f0429adc29687819cd79269089c653
John Hayes-Reed <john.hayes.reed@gmail.com>
2018-09-09 11:17:20 +0000

    Resolve "Add guard gem"

--- spec/factories/construction_records.rb
+++ spec/factories/construction_records.rb
@@ -4,8 +4,8 @@ FactoryBot.define do
     customer { 'TEST_CUSTOMER' }
     description { 'TEST_DESCRIPTION' }
     category { :civil }
-    started_on { Date.today - 3.days }
-    finished_on { Date.today - 1.day }
+    started_on { Date.new(2017, 2, 1) - 3.days }
+    finished_on { Date.new(2017, 2, 1) - 1.day }
 
     %i[title customer description category started_on finished_on ].each do |att|
       trait :"nil_#{att}" do
